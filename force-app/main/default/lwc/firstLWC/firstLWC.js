ccfb82a7d95c6feb52c27a052cd2fbffb7318b7e
Praveena <praveena.busam@gmail.com>
2025-03-12 12:52:19 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+    name ='Praveena';
+    company ='Praval';
+    desgination ='Devloper';
+    salary ='10000';
+}
